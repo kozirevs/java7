e3347509879495d35d6e7bb1baff73935ab076d7
akoz <kozirevs@inbox.lv>
2020-03-07 12:11:25 +0200

    added book, bookshelf classes and adjusted build

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,21 @@ public class App {
     static Logger log = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
+        Book book1 = new Book("1234567890", "Story Of My Life", "Aleksandrs", 100);
+        Book book2 = new Book("1234534566", "Story Of My Life", "Kozirevs", 200);
+        Book book3 = new Book("1234567889", "Story Of My Life", "Georgs", 300);
+        Book book4 = new Book("1234876543", "Story Of My Life", "User", 400);
+
+        Bookshelf bookshelf = new Bookshelf();
+        bookshelf.add(book1);
+        bookshelf.add(book2);
+        bookshelf.add(book3);
+        bookshelf.add(book4);
+
+        bookshelf.getBooks().forEach(book -> log.info(book.getTitle()));
+
+        System.out.println("\n\n****************************\n\n");
+
         try {
             throw new RuntimeException("Nereala kluda");
         } catch (RuntimeException e) {
